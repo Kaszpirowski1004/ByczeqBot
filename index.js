864de9b2c1c49b9cf55c1763d4a2d49c1cc0752b
Kaszpirowski1004 <przemek1004@wp.pl>
2020-07-06 13:25:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -46,12 +46,18 @@ client.on('message', function (message) {
 	}
 });
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 client.on('message', function (message) {
 	if (message.content === "!rzuc") {
 		
-		var a =0;
-		var b= 1;		
-		var number = Math.random() * (b - a) + a;
+		
+		var number = getRandomIntInclusive(0, 1);
 		if(number === 0)
 		{
 			message.channel.send("Reszka byczqu!");
